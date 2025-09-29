218e3b67f4917b3fdabe34aa72ce21c01afa5d55
Kieran White <kieran.white@bsqgroup.co.uk>
2025-09-29 15:22:35 +0100

    Missing dependency

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
 const { pool } = require("./db"); // adjust if you use knex
